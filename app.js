9b853fbe6e5134d470b42d25c63aab44dbdcac95
anahisbarrie <anahis.barrie@gmail.com>
2019-06-10 18:39:00 -0400

    final changes

--- app.js
+++ app.js
@@ -22,53 +22,81 @@ var nextArrival = 0;
 var minAway = 0;
 var firstTrainTime = 0;
 
+
 // capture information on button click
 
-$("#add-train").on("click", function(event){
-event.preventDefault();
+$("#add-train").on("click", function (event) {
+    event.preventDefault();
 
     var trainName = $("#InputTrainName").val().trim();
     var destination = $("#InputDestination").val().trim();
     var firstTrainTime = $("#InputFirstTrainTime").val().trim();
     var frecuency = $("#InputFrecuency").val().trim();
-    // var nextArrival = 
+    // var nextTrainArrival=
     // var minAway = 
-    
 
 
 
-// code to push to firebase database
 
-dataRef.ref().push({
+    // code to push to firebase database
 
-    trainName: trainName,
-    destination: destination,
-    frecuency: frecuency,
-    firstTrainTime: firstTrainTime
-    // NextArrival: nextArrival,
-    // MinutesAway: minAway,
-    // dateAdded: firebase.database.ServerValue.TIMESTAMP
-});
- alert("Train successfully added");
+    dataRef.ref().push({
+
+        trainName: trainName,
+        destination: destination,
+        frecuency: frecuency,
+        firstTrainTime: firstTrainTime
+        // NextArrival: nextTrainArrival,
+        // MinutesAway: minAway,
+        // dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+    alert("Train successfully added");
 });
 
 //Adds a train to the database and rows into the html
+//Current time
+var currentTime = new Date()
+nextArrival = currentTime - firstTrainTime
 
-dataRef.ref().on("child_added", function(childSnapshot){
+
+dataRef.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val().trainName);
     console.log(childSnapshot.val().destination);
     console.log(childSnapshot.val().frecuency);
     console.log(childSnapshot.val().firstTrainTime);
+    console.log(childSnapshot.val().nextTrainArrival);
 
-// Sote everything into a variable
+    // Store everything into a variable
     $("#DisplayTrainName").append("<div class='well'><span>" + childSnapshot.val().trainName + "</span></div>")
     $("#DisplayDestination").append("<div class='well'><span>" + childSnapshot.val().destination + "</span></div>")
     $("#DisplayFrecuency").append("<div class='well'><span>" + childSnapshot.val().frecuency + "</span></div>")
-    $("#DisplayNextArrival").append("<div class='well'><span>" + childSnapshot.val().firstTrainTime + "</span></div>")
-    var frecuency;
-    var firstTime = 0;
+    $("#DisplayNextArrival").append("<div class='well'><span>" + childSnapshot.val().nextTrainArrival + "</span></div>")
+    $("#DisplayMinsAway").append("<div class='well'><span>" + childSnapshot.val().minAway + "</span></div>")
 
+
+    var frecuency;
+    var firstTrainTime;
+    var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+    // console.log(firstTimeConverted);
+
+
+
+
+
+
+    //difference between the times
+    var differenceTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("diference in time" + differenceTime)
+    //time apart
+    // var tRemainer = differenceTime % frecuency;
+    // console.log(tRemainer);
+    //minute until train
+    // var tMinUntilTrain = frecuency - tRemainer;
+    // console.log ("min until train arrive" + tMinUntilTrain);
+    //next train
+    // var nextTrain = moment().add(tMinUntilTrain, "minutes")
     // Handle the errors
+
 }, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
 
